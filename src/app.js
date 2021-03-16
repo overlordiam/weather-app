c6c200753ea5545d69304ab369b5c0fb497444ef
suhaas <footballandgermany@gmail.com>
2021-03-16 15:22:07 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/weather-data')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define path for express config
 const publicDirectoryPath = path.join(__dirname, '../public') 
@@ -92,6 +93,6 @@ app.get('*', (req, res) => {
 
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server up and running')
 })
